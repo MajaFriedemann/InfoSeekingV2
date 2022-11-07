463851302f0d0283989238616167344fe6c1f319
Maja Friedemann <44875405+MajaFriedemann@users.noreply.github.com>
2022-11-07 14:26:53 +0000

    fix points

--- scripts/squircles.js
+++ scripts/squircles.js
@@ -537,7 +537,7 @@ function drawSquircles(parent, canvasID, canvasWidth, canvasHeight, squircleStai
                                     points = round(0.1*(Math.max(color_mean_level, color_mean_two_level) + 1),1);
                                     document.getElementById('confidence-question').innerHTML = '<h1 style="color: rgb(255,0,0); font-size: 5em; margin: -3vh auto 2vh; z-index: 100">+ '+ points + '</h1>';
                                 } else {
-                                    points = round(0.1*(10-((Math.min(color_mean_level, color_mean_two_level)))),1);
+                                    points = round(0.1*(100-((Math.min(color_mean_level, color_mean_two_level)))),1);
                                     document.getElementById('confidence-question').innerHTML = '<h1 style="color: rgb(0,100,255); font-size: 5em; margin: -3vh auto 2vh; z-index: 100">+ '+ points + '</h1>';
                                 }
                             } else {
@@ -545,7 +545,7 @@ function drawSquircles(parent, canvasID, canvasWidth, canvasHeight, squircleStai
                                     points = round(0.1*(Math.min(color_mean_level, color_mean_two_level) + 1),1);
                                     document.getElementById('confidence-question').innerHTML = '<h1 style="color: rgb(0,100,255); font-size: 5em; margin: -3vh auto 2vh; z-index: 100">+ '+ points + '</h1>';
                                 } else if (betterColor === "blue")  {
-                                    points = round(0.1*(10-((Math.max(color_mean_level, color_mean_two_level)))),1);
+                                    points = round(0.1*(100-((Math.max(color_mean_level, color_mean_two_level)))),1);
                                     document.getElementById('confidence-question').innerHTML = '<h1 style="color: rgb(255,0,0); font-size: 5em; margin: -3vh auto 2vh; z-index: 100">+ '+ points + '</h1>';
                                 }
                             }
@@ -555,7 +555,7 @@ function drawSquircles(parent, canvasID, canvasWidth, canvasHeight, squircleStai
                                     points = round(0.1*(Math.max(color_mean_level, color_mean_two_level) + 1),1);
                                     document.getElementById('confidence-question').innerHTML = '<h1 style="color: rgb(255,0,0); font-size: 5em; margin: -3vh auto 2vh; z-index: 100">+ '+ points + '</h1>';
                                 } else if (betterColor === "blue") {
-                                    points = round(0.1*(10-((Math.min(color_mean_level, color_mean_two_level)))),1);
+                                    points = round(0.1*(100-((Math.min(color_mean_level, color_mean_two_level)))),1);
                                     document.getElementById('confidence-question').innerHTML = '<h1 style="color: rgb(0,100,255); font-size: 5em; margin: -3vh auto 2vh; z-index: 100">+ '+ points + '</h1>';
                                 }
                             } else {
@@ -563,7 +563,7 @@ function drawSquircles(parent, canvasID, canvasWidth, canvasHeight, squircleStai
                                     points = round(0.1*(-(Math.min(color_mean_level, color_mean_two_level) + 1)),1);
                                     document.getElementById('confidence-question').innerHTML = '<h1 style="color: rgb(0,100,255); font-size: 5em; margin: -3vh auto 2vh; z-index: 100">- '+ Math.abs(points) + '</h1>'; // only using the minus in the string and then the absolute point value here because otherwise the minus is difficult to see!
                                 } else if (betterColor === "blue")  {
-                                    points = round(0.1*(-10-((Math.max(color_mean_level, color_mean_two_level)))),1);
+                                    points = round(0.1*(-100-((Math.max(color_mean_level, color_mean_two_level)))),1);
                                     document.getElementById('confidence-question').innerHTML = '<h1 style="color: rgb(255,0,0); font-size: 5em; margin: -3vh auto 2vh; z-index: 100">- '+ Math.abs(points) + '</h1>';
                                 }
                             }
